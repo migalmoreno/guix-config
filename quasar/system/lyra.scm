7c2d30ca4065347779c483a39a0708f9326acba9
Miguel Ángel Moreno <mail@migalmoreno.com>
2022-06-06 00:16:02 +0200

    system: lyra: Re-structures config and tweaks services

--- quasar/system/lyra.scm
+++ quasar/system/lyra.scm
@@ -8,8 +8,7 @@
   #:use-module (srfi srfi-1)
   #:use-module (gnu)
   #:use-module (gnu packages base)
-  #:use-module (gnu packages xorg)
-  #:use-module (gnu packages version-control)
+  #:use-module (gnu packages certs)
   #:use-module (gnu packages emacs)
   #:use-module (gnu packages emacs-xyz)
   #:use-module (gnu packages curl)
@@ -20,7 +19,9 @@
   #:use-module (gnu packages cryptsetup)
   #:use-module (gnu packages display-managers)
   #:use-module (gnu packages package-management)
-  #:use-module (gnu packages certs)
+  #:use-module (gnu packages suckless)
+  #:use-module (gnu packages xorg)
+  #:use-module (gnu packages version-control)
   #:use-module (gnu services cups)
   #:use-module (gnu services networking)
   #:use-module (gnu services desktop)
@@ -55,26 +56,92 @@
     "\n"
     "SUBSYSTEM==\"usb_device\", GROUP=\"spice\", MODE=\"0660\"")))
 
-(define %xorg-libinput-configuration
-  "Section \"InputClass\"
-  Identifier \"Touchpads\"
-  Driver \"libinput\"
-  MatchDevicePath \"/dev/input/event*\"
-  MatchIsTouchpad \"on\"
+(define lyra-users
+  (cons*
+   (user-account
+    (name "vega")
+    (comment "vega")
+    (group "users")
+    (home-directory "/home/vega")
+    (supplementary-groups
+     '("wheel"
+       "netdev"
+       "lp"
+       "audio"
+       "kvm"
+       "spice"
+       "libvirt"
+       "video")))
+   %base-user-accounts))
 
-  Option \"Tapping\" \"on\"
-  Option \"TappingDrag\" \"on\"
-  Option \"DisableWhileTyping\" \"on\"
-  Option \"MiddleEmulation\" \"on\"
-  Option \"ScrollMethdod\" \"twofinger\"
-EndSection
-Section \"InputClass\"
-  Identifier \"Keyboards\"
-  Driver \"libinput\"
-  MatchDevicePath \"/dev/input/event*\"
-  MatchIsKeyboard \"on\"
-EndSection
-")
+(define lyra-packages
+  (append
+    (list
+     emacs-no-x
+     nss-certs
+     cryptsetup
+     git
+     curl
+     gnu-make)
+    %base-packages))
+
+(define lyra-services
+  (cons* (screen-locker-service slock #:allow-empty-passwords? #f)
+         (service kernel-module-loader-service-type
+                  '("ddcci" "ddcci_backlight"))
+         (simple-service 'ddcci-config etc-service-type
+                         (list `("modprobe.d/ddcci.conf"
+                                 ,%ddcci-config)))
+         (bluetooth-service #:auto-enable? #t)
+         (service virtlog-service-type)
+         (service libvirt-service-type
+                  (libvirt-configuration
+                   (unix-sock-group "libvirt")
+                   (tls-port "16555")))
+         (extra-special-file "/usr/bin/env"
+                             (file-append coreutils "/bin/env"))
+         (extra-special-file "/bin/bash"
+                             (file-append bash "/bin/bash"))
+         (modify-services %desktop-services
+           (udev-service-type config =>
+                              (udev-configuration
+                               (inherit config)
+                               (rules (append
+                                        (udev-configuration-rules config)
+                                        (list %backlight-udev-rule
+                                              %spice-udev-rule)))))
+           (guix-service-type config =>
+                              (guix-configuration
+                               (inherit config)
+                               (substitute-urls
+                                (append (list "https://substitutes.nonguix.org")
+                                        %default-substitute-urls))
+                               (authorized-keys
+                                (append
+                                  (list
+                                   (local-file "../../keys/signatures/nonguix.pub"))
+                                  %default-authorized-guix-keys))))
+           (delete gdm-service-type))))
+
+(define lyra-mapped-devices
+  (list (mapped-device
+         (source
+          (uuid "0f74821b-da48-4f0c-9f94-f39e646da1bf"))
+         (target "system-root")
+         (type luks-device-mapping))))
+
+(define lyra-file-systems
+  (cons*
+   (file-system
+     (mount-point "/")
+     (device "/dev/mapper/system-root")
+     (type "ext4")
+     (dependencies lyra-mapped-devices))
+   (file-system
+     (device "/dev/nvme0n1p1")
+     (mount-point "/boot/efi")
+     (type "vfat"))
+   %base-file-systems))
 
 (define %system/lyra
   (operating-system
@@ -91,96 +158,15 @@ EndSection
        (name "spice")
        (system? #t))
       %base-groups))
-    (users (cons* (user-account
-                   (name "vega")
-                   (comment "vega")
-                   (group "users")
-                   (home-directory "/home/vega")
-                   (supplementary-groups
-                    '("wheel"
-                      "netdev"
-                      "lp"
-                      "audio"
-                      "kvm"
-                      "spice"
-                      "libvirt"
-                      "video")))
-                  %base-user-accounts))
-    (packages (append (list
-                       emacs-exwm
-                       emacs-no-x
-                       nss-certs
-                       cryptsetup
-                       guix-simplyblack-sddm-theme
-                       git
-                       curl
-                       gnu-make)
-                      %base-packages))
-    (services (cons* (service sddm-service-type
-                              (sddm-configuration
-                               (xorg-configuration
-                                (xorg-configuration
-                                 (keyboard-layout keyboard-layout)
-                                 (extra-config (list
-                                                %xorg-libinput-configuration))))
-
-                               (theme "guix-simplyblack-sddm")))
-                     (service kernel-module-loader-service-type
-                              '("ddcci" "ddcci_backlight"))
-                     (simple-service 'ddcci-config etc-service-type
-                                     (list `("modprobe.d/ddcci.conf"
-                                             ,%ddcci-config)))
-                     (bluetooth-service #:auto-enable? #t)
-                     (service virtlog-service-type)
-                     (service libvirt-service-type
-                              (libvirt-configuration
-                               (unix-sock-group "libvirt")
-                               (tls-port "16555")))
-                     (extra-special-file "/usr/bin/env"
-                                         (file-append coreutils "/bin/env"))
-                     (extra-special-file "/bin/bash"
-                                         (file-append bash "/bin/bash"))
-                     (modify-services %desktop-services
-                       (udev-service-type config =>
-                                          (udev-configuration
-                                           (inherit config)
-                                           (rules (append
-                                                    (udev-configuration-rules config)
-                                                    (list %backlight-udev-rule
-                                                          %spice-udev-rule)))))
-                       (guix-service-type config =>
-                                          (guix-configuration
-                                           (inherit config)
-                                           (substitute-urls
-                                            (append (list "https://substitutes.nonguix.org")
-                                                    %default-substitute-urls))
-                                           (authorized-keys
-                                            (append
-                                              (list
-                                               (local-file "../../keys/signatures/nonguix.pub"))
-                                              %default-authorized-guix-keys))))
-                       (delete gdm-service-type))))
+    (users lyra-users)
+    (packages lyra-packages)
+    (services lyra-services)
     (bootloader
      (bootloader-configuration
       (bootloader grub-efi-bootloader)
       (targets (list "/boot/efi"))
       (keyboard-layout keyboard-layout)))
-    (mapped-devices
-     (list (mapped-device
-            (source
-             (uuid "0f74821b-da48-4f0c-9f94-f39e646da1bf"))
-            (target "system-root")
-            (type luks-device-mapping))))
-    (file-systems
-     (cons* (file-system
-              (mount-point "/")
-              (device "/dev/mapper/system-root")
-              (type "ext4")
-              (dependencies mapped-devices))
-            (file-system
-              (device "/dev/nvme0n1p1")
-              (mount-point "/boot/efi")
-              (type "vfat"))
-            %base-file-systems))))
+    (mapped-devices lyra-mapped-devices)
+    (file-systems lyra-file-systems)))
 
 %system/lyra
