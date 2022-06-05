35756c10480cfc4b2ccbbaa6f2095e4025b4af12
Miguel Ángel Moreno <mail@migalmoreno.com>
2022-06-06 00:26:04 +0200

    efimerspan: home: xorg: Amends unclutter service

--- efimerspan/home/services/xorg.scm
+++ efimerspan/home/services/xorg.scm
@@ -2,19 +2,36 @@
   #:use-module (efimerspan home services glib)
   #:use-module (gnu services configuration)
   #:use-module (gnu packages)
-  #:use-module (guix packages)
   #:use-module (gnu services)
+  #:use-module (gnu services base)
   #:use-module (gnu home services)
   #:use-module (gnu packages xdisorg)
+  #:use-module (gnu packages xorg)
+  #:use-module (gnu packages gl)
   #:use-module (gnu home services shepherd)
+  #:use-module (gnu system keyboard)
+  #:use-module (gnu system setuid)
   #:use-module (guix gexp)
+  #:use-module (guix store)
+  #:use-module (guix packages)
+  #:use-module (guix records)
+  #:use-module (srfi srfi-1)
+  #:use-module (srfi srfi-9)
+  #:use-module (srfi srfi-26)
+  #:use-module (ice-9 match)
   #:export (home-unclutter-service-type
-            home-unclutter-configuration))
+            home-unclutter-configuration
+            xinitrc
+            xorg-start-command
+            xorg-configuration))
 
-(define-configuration home-unclutter-configuration
+(define-configuration/no-serialization home-unclutter-configuration
   (package
     (package unclutter)
-    "The @code{unclutter} package to use."))
+    "The @code{unclutter} package to use.")
+  (seconds
+   (integer 5)
+   "The number of idle seconds to wait for @code{unclutter} to remove the cursor."))
 
 (define (unclutter-shepherd-service config)
   (list
@@ -24,8 +41,9 @@
     (stop #~(make-kill-destructor))
     (start #~(make-forkexec-constructor
               (list
-               #$(file-append (home-unclutter-configuration-package config)) "/bin/unclutter"
-               "-idle" "2"))))))
+               #$(file-append (home-unclutter-configuration-package config) "/bin/unclutter")
+               "-idle"
+               #$(number->string (home-unclutter-configuration-seconds config))))))))
 
 (define home-unclutter-service-type
   (service-type
