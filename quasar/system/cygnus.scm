fd4aeefe0dfea0ce2adb084d3acb6130a21b14e7
Miguel Ángel Moreno <mail@migalmoreno.com>
2022-06-23 16:27:54 +0200

    system: cygnus: Adds nginx configuration for Synapse

--- quasar/system/cygnus.scm
+++ quasar/system/cygnus.scm
@@ -22,6 +22,11 @@
   #:export (%system/cygnus))
 
 (define %letsencrypt-dir "/etc/letsencrypt/live")
+(define %letsencrypt-acme-challenge
+  (nginx-location-configuration
+   (uri "/.well-known")
+   (body '("root /srv/http;"))))
+(define %domain (getenv "DOMAIN"))
 
 (define %nginx-deploy-hook
   (program-file
@@ -37,10 +42,10 @@
              (list
               (nginx-server-configuration
                (listen '("443 ssl http2"))
-               (server-name (list (getenv "DOMAIN") (string-append "www." (getenv "DOMAIN"))))
-               (root (string-append "/srv/http/" (getenv "DOMAIN")))
-               (ssl-certificate (string-append %letsencrypt-dir "/" (getenv "DOMAIN") "/fullchain.pem"))
-               (ssl-certificate-key (string-append %letsencrypt-dir "/" (getenv "DOMAIN") "/privkey.pem"))
+               (server-name (list %domain (string-append "www." %domain)))
+               (root (string-append "/srv/http/" %domain))
+               (ssl-certificate (string-append %letsencrypt-dir "/" %domain "/fullchain.pem"))
+               (ssl-certificate-key (string-append %letsencrypt-dir "/" %domain "/privkey.pem"))
                (locations
                 (list
                  (nginx-location-configuration
@@ -50,12 +55,19 @@
                          "client_body_temp_path /srv/client_temp;"
                          "dav_methods PUT DELETE MKCOL COPY MOVE;"
                          "create_full_put_path on;"
-                         "dav_access group:rw all:r;"))))))
+                         "dav_access group:rw all:r;")))
+                 %letsencrypt-acme-challenge
+                 (nginx-location-configuration
+                  (uri "/.well-known/matrix/server")
+                  (body
+                   (list "default_type application/json;"
+                         (format #f "return 200 '{\"m.server\": \"matrix.~a:443\"}';" %domain)
+                         "add_header Access-Control-Allow-Origin *;"))))))
               (nginx-server-configuration
                (listen '("443 ssl http2"))
-               (server-name (list (string-append "whoogle." (getenv "DOMAIN"))))
-               (ssl-certificate (string-append %letsencrypt-dir "/" (getenv "DOMAIN") "/fullchain.pem"))
-               (ssl-certificate-key (string-append %letsencrypt-dir "/" (getenv "DOMAIN") "/privkey.pem"))
+               (server-name (list (string-append "whoogle." %domain)))
+               (ssl-certificate (string-append %letsencrypt-dir "/" %domain "/fullchain.pem"))
+               (ssl-certificate-key (string-append %letsencrypt-dir "/" %domain "/privkey.pem"))
                (locations
                 (list
                  (nginx-location-configuration
@@ -63,7 +75,27 @@
                   (body
                    (list "proxy_pass http://localhost:5000/;"
                          "proxy_set_header X-Forwarded-For $remote_addr;"
-                         "proxy_set_header HOST $http_host;")))))))))))
+                         "proxy_set_header HOST $http_host;")))
+                 %letsencrypt-acme-challenge)))
+              (nginx-server-configuration
+               (listen '("443 ssl http2"
+                         "[::]:443 ssl http2"
+                         "8448 ssl http2 default_server"
+                         "[::]:8448 ssl http2 default_server"))
+               (server-name (list (string-append "matrix." %domain)))
+               (ssl-certificate (string-append %letsencrypt-dir "/" %domain "/fullchain.pem"))
+               (ssl-certificate-key (string-append %letsencrypt-dir "/" %domain "/privkey.pem"))
+               (locations
+                (list
+                 (nginx-location-configuration
+                  (uri "~ ^(/_matrix|/_synapse/client)")
+                  (body
+                   (list "proxy_pass http://localhost:8008;"
+                         "proxy_set_header X-Forwarded-For $remote_addr;"
+                         "proxy_set_header X-Forwarded-Proto $scheme;"
+                         "proxy_set_header Host $host;"
+                         "client_max_body_size 50M;")))
+                 %letsencrypt-acme-challenge))))))))
 
 (define %cygnus-services
   (append
@@ -72,24 +104,26 @@
     (service postgresql-service-type)
     (service synapse-service-type
              (synapse-configuration
-              (server-name (getenv "DOMAIN"))
+              (server-name %domain)
+              (enable-registration? #f)
+              (public-base-url (string-append "https://matrix." %domain))
               (shared-secret (getenv "CYGNUS_SYNAPSE_SHARED_SECRET"))
               (postgresql-db? #t)
               (postgresql-db-password (getenv "CYGNUS_SYNAPSE_DB_PASSWORD"))))
     (service dhcp-client-service-type)
     (service certbot-service-type
-             (let ((domain (getenv "DOMAIN")))
-               (certbot-configuration
-                (email (string-append "contact@" domain))
-                (webroot "/srv/http")
-                (certificates
-                 (list
-                  (certificate-configuration
-                   (name domain)
-                   (domains (list domain
-                                  (string-append "wwww." domain)
-                                  (string-append "whoogle." domain)))
-                   (deploy-hook %nginx-deploy-hook)))))))
+             (certbot-configuration
+              (email (string-append "contact@" %domain))
+              (webroot "/srv/http")
+              (certificates
+               (list
+                (certificate-configuration
+                 (name %domain)
+                 (domains (list %domain
+                                (string-append "wwww." %domain)
+                                (string-append "whoogle." %domain)
+                                (string-append "matrix." %domain)))
+                 (deploy-hook %nginx-deploy-hook))))))
     (service openssh-service-type
              (openssh-configuration
               (openssh openssh-sans-x)
