cdeed031513bee9e10a7fe4e14bf57dd3b37ca4c
Miguel Ángel Moreno <mail@migalmoreno.com>
2022-07-13 20:01:25 +0200

    etc: eb-mail: Adds Gnus helpers

--- etc/elibs/eb-mail.el
+++ etc/elibs/eb-mail.el
@@ -6,14 +6,18 @@
   "MUA and other email-related settings."
   :group 'eb)
 
+(defcustom eb-mail-gnus-topic-topology nil
+  "Topics topology for Gnus."
+  :group 'eb-mail
+  :type 'list)
+
+(defcustom eb-mail-gnus-topic-alist nil
+  "Alist of Gnus topics."
+  :group 'eb-mail
+  :type 'list)
+
 (defvar eb-mail-gnus-subscribe-groups-done nil
   "Only subscribe to groups once, else Gnus won't restart.")
-
-;;;###autoload
-(defun eb-mail-group-list-subscribed-groups ()
-  "Lists all the subscribed groups regardless of their unread messages status."
-  (interactive)
-  (gnus-group-list-all-groups 5))
 
 ;;;###autoload
 (defun eb-mail-org-mime-darken-codeblocks ()
@@ -26,6 +30,19 @@
   "Nicely offset block quotes in email bodies."
   (org-mime-change-element-style
    "blockquote" "border-left: 2px solid gray; padding-left: 4px;"))
+
+;;;###autoload
+(define-minor-mode eb-mail-gnus-topic-mode
+  "Custom mode to apply Gnus topics topology declaratively and subscribe
+automatically to the defined groups in them."
+  :global t :group 'eb-mail
+  (setq gnus-topic-topology eb-mail-gnus-topic-topology)
+  (setq gnus-topic-alist eb-mail-gnus-topic-alist)
+  (mapc (lambda (topic)
+          (gnus-subscribe-hierarchically topic))
+        (flatten-list (mapcar (lambda (topic)
+                                  (cdr topic))
+                                eb-mail-gnus-topic-alist))))
 
 ;;;###autoload
 (define-minor-mode eb-mail-message-mode
