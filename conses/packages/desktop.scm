82160580f00bdffd4ca1bcb8111d8edf703b84f0
Miguel Ángel Moreno <mail@migalmoreno.com>
2022-07-15 00:18:56 +0200

    packages: desktop: Updates bibata-cursor-theme

--- conses/packages/desktop.scm
+++ conses/packages/desktop.scm
@@ -5,28 +5,39 @@
   #:use-module (guix git-download)
   #:use-module (gnu packages xorg)
   #:use-module (gnu packages image)
+  #:use-module (gnu packages python)
+  #:use-module (gnu packages python-xyz)
+  #:use-module (gnu packages python-build)
   #:use-module ((guix licenses) #:prefix license:))
 
 (define-public bibata-cursor-theme
-  (package
-    (name "bibata-cursor-theme")
-    (version "1.1.2")
-    (source
-     (origin
-       (method git-fetch)
-       (uri (git-reference
-             (url "https://github.com/ful1e5/Bibata_Cursor")
-             (commit (string-append "v" version))))
-       (file-name (git-file-name name version))
-       (sha256
-        (base32 "0qksaxph5jnp1wsv54ad1n4rc6xwai3rxpkhhby7vpimk2qbiaf0"))))
-    (build-system gnu-build-system)
-    (inputs
-     (list libxcursor libpng))
-    (native-inputs
-     (list python-clickgen))
-    (home-page "https://github.com/ful1e5/Bibata_Cursor")
-    (synopsis "Open-source, compact, and material-designed cursor set.")
-    (description "Bibata is a open-source, compact, and material designed cursor set. This project
+  (let ((commit "8faa0631307c014e544bc857feed8c0300564d48")
+        (revision "0"))
+    (package
+      (name "bibata-cursor-theme")
+      (version (git-version "0" revision commit))
+      (source
+       (origin
+         (method git-fetch)
+         (uri (git-reference
+               (url "https://github.com/ful1e5/Bibata_Cursor")
+               (commit commit)))
+         (file-name (git-file-name name version))
+         (sha256
+          (base32 "1n6y8frpp0zayw8ya7y53wr0mnvam16y9hailx61v4jdij4krmkr"))))
+      (build-system gnu-build-system)
+      (inputs
+       (list libxcursor
+             libpng
+             libx11))
+      (native-inputs
+       (list
+        python
+        python-setuptools
+        python-wheel
+        python-clickgen))
+      (home-page "https://github.com/ful1e5/Bibata_Cursor")
+      (synopsis "Open-source, compact, and material-designed cursor set.")
+      (description "Bibata is a open-source, compact, and material designed cursor set. This project
 aims at improving the cursor experience.")
-    (license license:gpl3)))
+      (license license:gpl3))))
