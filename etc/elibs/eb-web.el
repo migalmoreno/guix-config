a503f7cde0b441f09e124dc2f059f53b2b4b7f25
Miguel Ángel Moreno <mail@migalmoreno.com>
2022-07-24 10:58:24 +0200

    etc: eb-web: Splits out vc helpers and amends Nyxt capture

--- etc/elibs/eb-web.el
+++ etc/elibs/eb-web.el
@@ -31,11 +31,6 @@
   :type 'integer
   :group 'eb-web)
 
-(defcustom eb-web-srht-token nil
-  "The OAuth2 token for the user account."
-  :type 'string
-  :group 'eb-web)
-
 (defvar eb-web-nyxt-development-flags
   (when (and (file-exists-p
               (expand-file-name
@@ -54,69 +49,6 @@
 
 (defvar eb-web-nyxt-process nil
   "Holds the current Nyxt process.")
-
-(defun eb-web-srht-repo-id (name)
-  "Return the ID associated with the sourcehut repository NAME."
-    (interactive "sRepo name: ")
-    (let* ((srht-token eb-web-srht-token)
-           (oauth2-token (concat "Bearer " srht-token))
-           (id (assoc-default
-                'id
-                (assoc-default
-                 'repository
-                 (assoc-default
-                  'me
-                  (assoc-default
-                   'data
-                   (request-response-data
-                    (request
-                      "https://git.sr.ht/query"
-                      :params `(("query" . ,(concat "query { me { repository(name:\"" name "\") { id } } }")))
-                      :type "GET"
-                      :headers `(("Authorization" . ,oauth2-token))
-                      :parser 'json-read
-                      :sync t
-                      :timeout 2
-                      :error (cl-function
-                              (lambda (&key error-thrown &allow-other-keys) (message "Error %S" error-thrown)))))))))))
-      id))
-
-(defun eb-web-srht-set-readme (id)
-  "Export the current file to html and set the result as readme for the
-  sourcehut repo identified by ID."
-  (interactive
-   (list (if-let* ((project (project-current t))
-                   (dir (project-root project))
-                   (name (string-match (rx (: "/" (group (+ (not "/"))) "/" eol)) dir)))
-             (eb-web-srht-repo-id (match-string 1 dir))
-             (call-interactively #'eb-web-srht-repo-id))))
-  (let* ((srht-token eb-web-srht-token)
-         (oauth2-token (concat "Bearer " srht-token))
-         (readme (if (derived-mode-p 'html-mode)
-                     (buffer-substring-no-properties (point-min) (point-max))
-                     (org-export-as (org-export-get-backend 'html) nil nil t)))
-         (json-object-type 'hash-table)
-         (json-array-type 'list)
-         (json-key-type 'string)
-         (query (make-hash-table))
-         (variables (make-hash-table)))
-    (puthash "id" id variables)
-    (puthash "readme" readme variables)
-    (puthash
-     "query"
-     "mutation UpdateRepo($id: Int!, $readme: String!) {
-      updateRepository(id: $id, input: { readme: $readme }) { id }
-    }"
-     query)
-    (puthash "variables" variables query)
-    (request
-      "https://git.sr.ht/query"
-      :type "POST"
-      :data (json-serialize query)
-      :headers `(("Content-Type" . "application/json") ("Authorization" . ,oauth2-token))
-      :parser 'json-read
-      :complete (cl-function (lambda (&key symbol-status &allow-other-keys)
-                               (message "Set: %S" symbol-status))))))
 
 ;;;###autoload
 (defun eb-web--bookmark-make-record (url title)
@@ -269,6 +201,20 @@ process is not found, it will automatically create one and connect Slynk to it."
      :type "nyxt"
      :link (eb-web-sly-eval '(nyxt:render-url (nyxt:url (nyxt:current-buffer))))
      :description (eb-web-sly-eval '(nyxt:title (nyxt:current-buffer))))))
+
+(cl-defun eb-web-nyxt-capture (template &key (roam-p nil))
+  "Stores and captures the current Nyxt page link in the corresponding
+ Org capture TEMPLATE, or if ROAM-P, in the corresponding Org Roam capture
+template."
+  (interactive)
+  (with-current-buffer (car (cl-remove-if-not (lambda (buffer)
+                                                (string-match (rx (: "Nyxt:"))
+                                                              (buffer-name buffer)))
+                                              (buffer-list)))
+    (org-store-link t)
+    (if roam-p
+        (org-roam-capture nil template)
+      (org-capture nil template))))
 
 ;;;###autoload
 (defun eb-web-connect-to-slynk ()
