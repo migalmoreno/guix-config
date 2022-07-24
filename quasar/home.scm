19a7f74dfebd5b7a9fd60c7eb1a50be051deb39f
Miguel Ángel Moreno <mail@migalmoreno.com>
2022-07-24 11:17:11 +0200

    home: Fixes channel root

--- quasar/home.scm
+++ quasar/home.scm
@@ -4,9 +4,10 @@
             boolean->one-or-zero))
 
 (define %channel-root
-  (find (lambda (path)
-          (file-exists? (string-append path "/.guix-channel")))
-        %load-path))
+  (canonicalize-path
+   (find (lambda (path)
+           (file-exists? (string-append path "/.guix-channel")))
+         %load-path)))
 
 (define (boolean->one-or-zero bool)
   "Convert a boolean BOOL to 1 or 0."
