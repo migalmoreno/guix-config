57bda44d492abae68ae8d35b3ac940780ab474ed
Miguel Ángel Moreno <mail@migalmoreno.com>
2022-07-27 08:08:48 +0200

    quasar: home: golang: Add go home service

---
+++ quasar/home/services/golang.scm
@@ -0,0 +1,21 @@
+(define-module (quasar home services golang)
+  #:use-module (conses home services emacs)
+  #:use-module (conses packages emacs-xyz)
+  #:use-module (gnu home-services base)
+  #:use-module (gnu packages golang)
+  #:use-module (gnu packages emacs-xyz)
+  #:export (go-service))
+
+(define (go-service)
+  (list
+   (home-generic-service 'home-go-packages #:packages (list go))
+   (elisp-configuration-service
+    '((add-to-list 'auto-mode-alist '("\\.go\\'" . go-mode))
+      (with-eval-after-load 'org
+        (require 'ob-go)
+        (add-to-list 'org-structure-template-alist '("go" . "src go"))
+        (add-to-list 'org-babel-load-languages '(go . t)))
+      (with-eval-after-load 'ob-core
+        (setq org-babel-default-header-args:go
+              '((:imports . "fmt")))))
+    #:elisp-packages (list emacs-go-mode emacs-ob-go))))
