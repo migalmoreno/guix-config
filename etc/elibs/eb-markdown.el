d320c66684a36309a74a95ba13880152bde3137b
Miguel Ángel Moreno <mail@migalmoreno.com>
2022-08-22 00:30:21 +0200

    etc: eb-markdown: Add personal Markdown customizations

---
+++ etc/elibs/eb-markdown.el
@@ -0,0 +1,15 @@
+(defgroup eb-markdown nil
+  "Markdown mode customizations."
+  :group 'eb)
+
+(define-minor-mode eb-markdown-minimal-mode
+  "Set a more minimal interface to Markdown mode."
+  :global t :group 'eb-markdown
+  (if eb-markdown-minimal-mode
+      (progn
+        (display-line-numbers-mode -1)
+        (visual-line-mode 1))
+    (display-line-numbers-mode 1)
+    (visual-line-mode -1)))
+
+(provide 'eb-markdown)
