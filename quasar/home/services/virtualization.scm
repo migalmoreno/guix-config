363f5ebeb8c2c2754da6c12be4ab32721cea2b32
Miguel Ángel Moreno <mail@migalmoreno.com>
2022-08-22 00:28:10 +0200

    quasar: home: virtualization: Add basic Docker setup

--- quasar/home/services/virtualization.scm
+++ quasar/home/services/virtualization.scm
@@ -1,7 +1,9 @@
 (define-module (quasar home services virtualization)
+  #:use-module (conses home services emacs)
   #:use-module (gnu home-services base)
   #:use-module (gnu home services xdg)
   #:use-module (gnu packages virtualization)
+  #:use-module (gnu packages emacs-xyz)
   #:use-module (gnu packages linux)
   #:use-module (gnu packages disk)
   #:use-module (gnu packages vpn)
@@ -25,4 +27,8 @@
                        (name "Virtual Machine Manager")
                        (type 'application)
                        (config
-                        '((exec . "virt-manager --connect qemu:///session"))))))))))
+                        '((exec . "virt-manager --connect qemu:///session"))))))))
+   (elisp-configuration-service
+    '((add-to-list 'auto-mode-alist '("Dockerfile\\'" . dockerfile-mode))
+      (put 'dockerfile-image-name 'safe-local-variable 'stringp))
+    #:elisp-packages (list emacs-dockerfile-mode))))
