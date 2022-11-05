b070ba6b07e9c096e360fefe19903414d98d47e6
Miguel Ángel Moreno <mail@migalmoreno.com>
2022-11-05 16:11:44 +0100

    matrix: emacs-ement: Add org hook for composing messages

--- conses/features/matrix.scm
+++ conses/features/matrix.scm
@@ -232,6 +232,7 @@ daemon for Matrix clients."
                                     't
                                   'nil)))
                   (get-value 'matrix-accounts config))))
+        (add-hook 'ement-room-compose-hook 'ement-room-compose-org)
         (with-eval-after-load 'ement
           (let ((map ement-room-mode-map))
             (define-key map "c" 'ement-room-compose-message)
