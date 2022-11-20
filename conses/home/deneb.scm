64790ae84eb9f205cd831cade4a56104451769e2
Miguel Ángel Moreno <mail@migalmoreno.com>
2022-11-20 01:21:26 +0100

    deneb: Add home features.

---
+++ conses/home/deneb.scm
@@ -0,0 +1,19 @@
+(define-module (conses home deneb)
+  #:use-module (conses features matrix)
+  #:use-module (conses packages matrix)
+  #:use-module (rde features base)
+  #:use-module (gnu services)
+  #:use-module (gnu services networking))
+
+(define-public %home-features
+  (list
+   (feature-user-info
+    #:user-name "deneb"
+    #:full-name (getenv "MAIL_PERSONAL_FULLNAME")
+    #:email (getenv "MAIL_PERSONAL_EMAIL"))
+   (feature-desktop-services
+    #:default-desktop-system-services
+    (modify-services (@@ (rde features base) %rde-desktop-system-services)
+      (delete network-manager-service-type)))
+   (feature-pantalaimon
+    #:pantalaimon pantalaimon-next)))
