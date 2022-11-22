a1091b0d08e1b61132b5fdca2d52538837dd39fd
Miguel Ángel Moreno <mail@migalmoreno.com>
2022-11-22 23:39:33 +0100

    packages: desktop: bibata-cursor-theme: Update.

--- conses/packages/desktop.scm
+++ conses/packages/desktop.scm
@@ -1,43 +1,28 @@
 (define-module (conses packages desktop)
-  #:use-module (conses packages python-xyz)
-  #:use-module (gnu packages xorg)
-  #:use-module (gnu packages image)
-  #:use-module (gnu packages python)
-  #:use-module (gnu packages python-xyz)
-  #:use-module (gnu packages python-build)
+  #:use-module (guix gexp)
   #:use-module (guix packages)
-  #:use-module (guix git-download)
-  #:use-module (guix build-system gnu)
+  #:use-module (guix download)
+  #:use-module (guix build-system copy)
   #:use-module ((guix licenses) #:prefix license:))
 
 (define-public bibata-cursor-theme
-  (let ((commit "8faa0631307c014e544bc857feed8c0300564d48")
-        (revision "0"))
-    (package
-      (name "bibata-cursor-theme")
-      (version (git-version "0" revision commit))
-      (source
-       (origin
-         (method git-fetch)
-         (uri (git-reference
-               (url "https://github.com/ful1e5/Bibata_Cursor")
-               (commit commit)))
-         (file-name (git-file-name name version))
-         (sha256
-          (base32 "1n6y8frpp0zayw8ya7y53wr0mnvam16y9hailx61v4jdij4krmkr"))))
-      (build-system gnu-build-system)
-      (inputs
-       (list libxcursor
-             libpng
-             libx11))
-      (native-inputs
-       (list
-        python
-        python-setuptools
-        python-wheel
-        python-clickgen))
-      (home-page "https://github.com/ful1e5/Bibata_Cursor")
-      (synopsis "Open-source, compact, and material-designed cursor set.")
-      (description "Bibata is a open-source, compact, and material designed cursor set. This project
+  (package
+   (name "bibata-cursor-theme")
+   (version "2.0.2")
+   (source
+    (origin
+     (method url-fetch/tarbomb)
+     (uri (string-append "https://github.com/ful1e5/Bibata_Cursor/releases/download/v"
+                         version "/Bibata.tar.gz"))
+     (file-name "Bibata.tar.gz")
+     (sha256
+      (base32 "0a6dr10hgd4g8cxi8ixbbkfzbf91hzds19yiffxdyyi2a6ic9pvh"))))
+   (build-system copy-build-system)
+   (arguments
+    `(#:install-plan
+      `(("." "share/icons"))))
+   (home-page "https://github.com/ful1e5/Bibata_Cursor")
+   (synopsis "Open-source, compact, and material-designed cursor set.")
+   (description "Bibata is a open-source, compact, and material designed cursor set. This project
 aims at improving the cursor experience.")
-      (license license:gpl3))))
+   (license license:gpl3)))
