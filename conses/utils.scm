5e1f4073cf09bf77b18469661fb5fe9efde3d691
Miguel Ángel Moreno <mail@migalmoreno.com>
2023-01-12 10:47:31 +0100

    conses: utils: Move predicates to feature-specific files.

--- conses/utils.scm
+++ conses/utils.scm
@@ -1,23 +1,15 @@
 (define-module (conses utils)
   #:use-module (gnu packages)
   #:use-module (guix gexp)
-  #:use-module (srfi srfi-1)
-  #:export (project-file))
+  #:use-module (srfi srfi-1))
 
 (define-public (maybe-procedure? x)
   (or (procedure? x) (not x)))
 
-(define-public (maybe-number? x)
-  (or (number? x) (not x)))
-
-(define-public (number-or-list? x)
-  (or (number? x) (list? x)))
-
-(define-public (maybe-pair-or-string? x)
-  (or (pair? x) (string? x) (not x)))
-
-(define-public (maybe-list-of-file-likes? x)
-  (or (and (list? x) (every file-like? x)) (not x)))
+(define-public (make-feature-list . features)
+  (append-map (lambda (f)
+                (if (list? f) f (list f)))
+              features))
 
 (define-public %project-root
   (canonicalize-path
@@ -28,5 +20,5 @@
                                           "conses"))))
          %load-path)))
 
-(define (project-file subpath)
+(define-public (project-file subpath)
   (local-file (string-append %project-root "/" subpath)))
