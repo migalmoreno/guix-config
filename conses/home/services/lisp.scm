d60e34b26a0eac6a37ad95a36e6d402d7d7e5283
Miguel Ángel Moreno <mail@migalmoreno.com>
2023-01-31 18:17:18 +0100

    home: lisp: Prepare for upstreaming.

--- conses/home/services/lisp.scm
+++ conses/home/services/lisp.scm
@@ -1,18 +1,24 @@
 (define-module (conses home services lisp)
+  #:use-module (rde serializers lisp)
   #:use-module (gnu services)
   #:use-module (gnu services configuration)
   #:use-module (gnu home services)
   #:use-module (gnu packages lisp)
+  #:use-module (guix diagnostics)
   #:use-module (guix gexp)
   #:use-module (guix packages)
-  #:use-module (rde serializers lisp)
-  #:export (lisp-config?
-            serialize-lisp-config
-            home-lisp-configuration
+  #:use-module (guix ui)
+  #:use-module (srfi srfi-1)
+  #:use-module (ice-9 match)
+  #:export (home-lisp-configuration
             home-lisp-service-type))
 
-(define-public lisp-config? sexp-config?)
-(define-public serialize-lisp-config serialize-sexp-config)
+(define lisp-config? sexp-config?)
+(define lisp-serialize sexp-serialize)
+(define serialize-lisp-config serialize-sexp-config)
+
+(define file-likes? (list-of file-like?))
+(define serialize-file-likes empty-serializer)
 
 (define-configuration home-lisp-configuration
   (lisp
@@ -20,18 +26,53 @@
     "The Lisp implementation to use.")
   (slynk-lisp
    (lisp-config '())
-   "List of expressions, where each expression can be Sexp or a Gexp. Sexp is a Lisp form.
-Strings don't require any conversion, but booleans do.
-Sexps can contain file-like objects, a string with path to a corresponding file in the store.
-Gexps should be strings and their value will be appended to the resulting Lisp file.
+   "List of expressions, where each expression can be a Sexp or a Gexp. Sexp is
+a Lisp form.  Strings don't require any conversion, but booleans do.
+Sexps can contain file-like objects, paths to corresponding files in the
+store that will be serialized as strings.  Gexps should be string-valued and
+their value will be appended to the resulting Lisp file.
 
-The list of expressions will be interposed with \n and everything will end up in the
-@file{slynk.lisp}.")
+The list of expressions will be interposed with \n and everything will end up
+in the @file{slynk.lisp}.")
   (sbclrc-lisp
    (lisp-config '())
-   "As per @code{slynk-lisp}, but everything will be placed in @file{sbclrc.lisp}."))
+   "As per @code{slynk-lisp}, but everything will be placed in
+@file{sbclrc.lisp}.")
+  (extra-source-registry-files
+   (file-likes '())
+   "List of file-likes to add under
+@file{.config/common-lisp/source-registry.conf.d} to allow the ASDF
+source-registry mechanism to find new Lisp systems in custom filesystem
+locations."))
 
 (define (home-lisp-files-service config)
+  (define (extra-source-registry-files->file-union files)
+    "Return a G-exp obtained by processing EXTRA-SOURCE-REGISTRY-FILES
+with FILE-UNION."
+
+    (define (file-like->name file)
+      (match file
+        ((? local-file?)
+         (local-file-name file))
+        ((? plain-file?)
+         (plain-file-name file))
+        ((? computed-file?)
+         (computed-file-name file))
+        (_ (leave (G_ "~a is not a local-file, plain-file or \
+computed-file object~%") file))))
+
+    (define (assert-conf-file-name name)
+      (unless (string-suffix? ".conf" name)
+        (display "Not a conf file")
+        (leave (G_ "`~a' lacks the required `.conf' file name extension~%")
+               name))
+      name)
+
+    (let ((labels (map (compose assert-conf-file-name
+                                file-like->name)
+                       files)))
+      (file-union "source-registry.conf.d" (zip labels files))))
+
   (define (filter-fields field)
     (filter-configuration-fields home-lisp-configuration-fields
                                  (list field)))
@@ -46,18 +87,23 @@ The list of expressions will be interposed with \n and everything will end up in
                      (string-drop-right (symbol->string field) 5)
                      ".lisp"))
           (field-obj (car (filter-fields field))))
-      (if (not (null? ((configuration-field-getter field-obj) config)))
+      (if (null? ((configuration-field-getter field-obj) config))
+          '()
           `(,(string-append "." filename)
-                  ,(mixed-text-file
-                    filename
-                    (serialize-field field)))
-          '())))
+            ,(mixed-text-file
+              filename
+              (serialize-field field))))))
 
   (filter
    (compose not null?)
    (list
     (file-if-not-empty 'sbclrc-lisp)
-    (file-if-not-empty 'slynk-lisp))))
+    (file-if-not-empty 'slynk-lisp)
+    (if (null? (home-lisp-configuration-extra-source-registry-files config))
+        '()
+        `(".config/common-lisp/source-registry.conf.d"
+          ,(extra-source-registry-files->file-union
+            (home-lisp-configuration-extra-source-registry-files config)))))))
 
 (define (home-lisp-profile-service config)
   (list (home-lisp-configuration-lisp config)))
