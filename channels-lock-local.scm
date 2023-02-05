566c96777bde58f994bbc2f06cabfb8ecd8d615b
Miguel Ángel Moreno <mail@migalmoreno.com>
2023-02-05 11:42:25 +0100

    channels-local: Add local channels files.

---
+++ channels-lock-local.scm
@@ -0,0 +1,33 @@
+(list (channel
+        (name 'nonguix)
+        (url "https://gitlab.com/nonguix/nonguix")
+        (branch "master")
+        (commit
+          "1ffeebab4743263053d15c473395379d51f7b0b0")
+        (introduction
+          (make-channel-introduction
+            "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
+            (openpgp-fingerprint
+              "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5"))))
+      (channel
+        (name 'guix)
+        (url "https://git.savannah.gnu.org/git/guix")
+        (branch "master")
+        (commit
+          "6f0c9053244d6b4cfc4130c963a15dbf83cbcdac")
+        (introduction
+          (make-channel-introduction
+            "9edb3f66fd807b096b48283debdcddccfea34bad"
+            (openpgp-fingerprint
+              "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA"))))
+      (channel
+        (name 'rde)
+        (url "file:///home/vega/src/guile/rde")
+        (branch "master")
+        (commit
+          "c973c2b4f983f327acb828c454d777c0677ecbbf")
+        (introduction
+          (make-channel-introduction
+            "257cebd587b66e4d865b3537a9a88cccd7107c95"
+            (openpgp-fingerprint
+              "2841 9AC6 5038 7440 C7E9  2FFA 2208 D209 58C1 DEB0")))))
