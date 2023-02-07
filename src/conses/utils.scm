a908cf3370b2a1d574bc34ca96421a268375c883
Miguel Ángel Moreno <mail@migalmoreno.com>
2023-02-07 22:47:52 +0100

    utils: Remove maybe-procedure? since it's upstreamed.

--- src/conses/utils.scm
+++ src/conses/utils.scm
@@ -1,10 +1,6 @@
 (define-module (conses utils)
-  #:use-module (gnu packages)
   #:use-module (guix gexp)
   #:use-module (srfi srfi-1))
-
-(define-public (maybe-procedure? x)
-  (or (procedure? x) (not x)))
 
 (define-public (make-feature-list . features)
   (append-map (lambda (f)
