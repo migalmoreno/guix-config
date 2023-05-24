03626727a46414df1335bcd329531c6199568ee8
Miguel Ángel Moreno <mail@migalmoreno.com>
2023-05-24 22:42:00 +0200

    channels-local: Replace local nonguix channel for upstream.

--- rde/channels-local.scm
+++ rde/channels-local.scm
@@ -13,5 +13,12 @@
        (branch "base-to-upstream"))
       (channel
        (name 'nonguix)
-       (url (format #f "file://~a/src/guile/nonguix" (getenv "HOME")))
-       (branch "base-to-upstream")))
+       (url "https://gitlab.com/nonguix/nonguix")
+       (branch "master")
+       (commit
+        "026b5b5c47f606161113398cda710a7c85164937")
+       (introduction
+        (make-channel-introduction
+         "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
+         (openpgp-fingerprint
+          "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5")))))
