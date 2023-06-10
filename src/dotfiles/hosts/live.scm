09aac9e3bf41c4a19d39bba8dcab967bd2225953
Miguel Ángel Moreno <mail@migalmoreno.com>
2023-06-10 14:16:42 +0200

    live: Refine features.

--- src/dotfiles/hosts/live.scm
+++ src/dotfiles/hosts/live.scm
@@ -1,20 +1,34 @@
 (define-module (dotfiles hosts live)
-  #:use-module (rde packages)
+  #:use-module (dotfiles utils)
+  #:use-module (rde features system)
   #:use-module (rde features base)
+  #:use-module (rde system services admin)
+  #:use-module (rde system services guix)
+  #:use-module (rde packages)
+  #:use-module (gnu services)
   #:use-module (gnu system)
   #:use-module (gnu system install))
 
-
-;;; Host-specific features
-
-(define-public %host-features
+(define live-extra-services
   (list
+   (simple-service
+    'sudoers-extra-service
+    sudoers-service-type
+    (list "%wheel ALL=(ALL) NOPASSWD: ALL"))
+   (service cow-store-service-type)))
+
+(define-public %live-features
+  (list
+   (feature-host-info
+    #:host-name "live"
+    #:timezone %default-timezone)
    (feature-base-packages
+    #:base-home-packages '()
     #:system-packages
     (strings->packages
      "make" "gnupg" "curl" "cryptsetup"
      "emacs-no-x" "parted" "git" "dosfstools"))
+   (feature-file-systems
+    #:file-systems (operating-system-file-systems installation-os))
    (feature-custom-services
-    #:system-services
-    (list
-     (operating-system-user-services installation-os)))))
+    #:system-services live-extra-services)))
