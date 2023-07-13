1b218f133536a71525b0f735281c51504fb5a55c
Miguel Ángel Moreno <mail@migalmoreno.com>
2023-07-13 10:47:34 +0200

    rde: Add home-service-dwl-guile channel.

--- rde/channels-local.scm
+++ rde/channels-local.scm
@@ -19,4 +19,13 @@
         (make-channel-introduction
          "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
          (openpgp-fingerprint
-          "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5")))))
+          "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5"))))
+      (channel
+       (name 'home-service-dwl-guile)
+       (url "https://github.com/engstrand-config/home-service-dwl-guile")
+       (branch "main")
+       (introduction
+        (make-channel-introduction
+         "314453a87634d67e914cfdf51d357638902dd9fe"
+         (openpgp-fingerprint
+          "C9BE B8A0 4458 FDDF 1268 1B39 029D 8EB7 7E18 D68C")))))
