8e6387966795fcb35542ea14b635d610b67d3e95
Miguel Ángel Moreno <mail@migalmoreno.com>
2023-08-20 13:11:26 +0200

    rde: Re-introduce channels-local lock file.

---
+++ rde/channels-lock-local.scm
@@ -0,0 +1,56 @@
+(list (channel
+        (name 'farg)
+        (url "https://github.com/engstrand-config/farg")
+        (branch "main")
+        (commit
+          "2ac3d59773730882b6f77fc9723e292a7ebdfa05"))
+      (channel
+        (name 'home-service-dtao-guile)
+        (url "https://github.com/engstrand-config/home-service-dtao-guile")
+        (branch "main")
+        (commit
+          "ad32455058d588452519edf49f761eb298c9e9bd")
+        (introduction
+          (make-channel-introduction
+            "64d0b70c547095ddc840dd07424b9a46ccc2e64e"
+            (openpgp-fingerprint
+              "C9BE B8A0 4458 FDDF 1268  1B39 029D 8EB7 7E18 D68C"))))
+      (channel
+        (name 'home-service-dwl-guile)
+        (url "https://github.com/engstrand-config/home-service-dwl-guile")
+        (branch "main")
+        (commit
+          "179ce7f0cb8db17987e876422d86b6d4e3717025")
+        (introduction
+          (make-channel-introduction
+            "314453a87634d67e914cfdf51d357638902dd9fe"
+            (openpgp-fingerprint
+              "C9BE B8A0 4458 FDDF 1268  1B39 029D 8EB7 7E18 D68C"))))
+      (channel
+        (name 'nonguix)
+        (url "https://gitlab.com/nonguix/nonguix")
+        (branch "master")
+        (commit
+          "d44f0cde50ca96eabf6e7b0f0c9bdbd89508b790")
+        (introduction
+          (make-channel-introduction
+            "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
+            (openpgp-fingerprint
+              "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5"))))
+      (channel
+        (name 'guix)
+        (url "file:///home/vega/src/guile/guix")
+        (branch "base-to-upstream")
+        (commit
+         "f42e9a875f38aa42a6b13bf2857a551ad5c9b218"))
+      (channel
+        (name 'rde)
+        (url "file:///home/vega/src/guile/rde")
+        (branch "base-to-upstream")
+        (commit
+          "fdea5c5ae04222f5eb5ded11dddec7d412faa672")
+        (introduction
+          (make-channel-introduction
+            "257cebd587b66e4d865b3537a9a88cccd7107c95"
+            (openpgp-fingerprint
+              "2841 9AC6 5038 7440 C7E9  2FFA 2208 D209 58C1 DEB0")))))
